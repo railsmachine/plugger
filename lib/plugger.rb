d74ef903f798c04082ec96263275862a41678837
Kevin Lawver <lawver@gmail.com>
2013-12-12 12:41:52 -0500

    Adds the plugin method used by moonshine generators.

--- lib/plugger.rb
+++ lib/plugger.rb
@@ -3,7 +3,36 @@ class Plugger
   def self.hi
     puts "Hi, I'm plugger. This is just a stub."
   end
-  
+    
+end
+
+module Rails
+  module Generators
+    
+    module Actions
+      
+      def plugin(name, options)
+        log :plugin, name
+
+        if options[:git] && options[:submodule]
+          options[:git] = "-b #{options[:branch]} #{options[:git]}" if options[:branch]
+          in_root do
+            run "git submodule add #{options[:git]} vendor/plugins/#{name}", :verbose => false
+          end
+        elsif options[:git] || options[:svn]
+          options[:git] = "-b #{options[:branch]} #{options[:git]}"   if options[:branch]
+          options[:svn] = "-r #{options[:revision]} #{options[:svn]}" if options[:revision]
+          in_root do
+            `bundle exec plugger install #{options[:svn] || options[:git]} --force`
+          end
+        else
+          log "! no git or svn provided for #{name}. Skipping..."
+        end
+      end
+      
+    end
+    
+  end
 end
 
 require 'rails/generators'
